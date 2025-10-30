aef6621ae14fed369c4d47c7a057b032cd1ed09a
JiHyoung You <fbwlgud51@gmail.com>
2025-10-30 16:01:58 +0900

    setting: json 서버 배포

--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -10,7 +10,8 @@ const ProductAll = () => {
   }, []);
 
   const getProducts = async () => {
-    let url = "http://localhost:4000/products";
+    let url =
+      " https://my-json-server.typicode.com/JiHy0ung/Givenchy-Clone/products";
     let response = await fetch(url);
     let data = await response.json();
 
